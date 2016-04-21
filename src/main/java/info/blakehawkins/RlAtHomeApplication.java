0bcec459bf4f65812be0dafc1f7e313db22b7bd1
Blake Hawkins <b.hawkins@sms.ed.ac.uk>
2016-04-21 14:19:46 +0100

    add boilerplate spring code

---
+++ src/main/java/info/blakehawkins/RlAtHomeApplication.java
@@ -0,0 +1,12 @@
+package info.blakehawkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlAtHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlAtHomeApplication.class, args);
+	}
+}
