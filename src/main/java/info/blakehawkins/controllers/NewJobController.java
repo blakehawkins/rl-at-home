ebca8b70c145daa8c8d18fcd8496962e9fdf1276
Blake Hawkins <blake.hawkins.11@gmail.com>
2016-04-23 16:25:58 +0100

    update controller post param metadata

--- src/main/java/info/blakehawkins/controllers/NewJobController.java
+++ src/main/java/info/blakehawkins/controllers/NewJobController.java
@@ -1,18 +1,27 @@
 package info.blakehawkins.controllers;
 
 import info.blakehawkins.JobSpec;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class NewJobController
 {
-    @RequestMapping("/new_job")
-    public @ResponseBody
-    JobSpec newJob(@RequestBody final Optional<String> jobName)
+    private CassandraTemplate template;
+
+    @Autowired
+    public NewJobController(CassandraTemplate template)
+    {
+        this.template = template;
+    }
+
+    @RequestMapping(value = "/new_job", method = RequestMethod.POST)
+    public JobSpec newJob(@RequestBody final Optional<String> jobName)
     {
         // If new job data has been passed, generate just a key.
         return new JobSpec();
